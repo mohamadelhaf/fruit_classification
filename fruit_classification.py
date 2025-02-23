f31446918b55740383ef82d289ee25ca7f42c2b0
mohamad elhaf <mhd_elhaf659@outlook.com>
2025-02-23 18:14:50 +0100

    testing

--- fruit_classification.py
+++ fruit_classification.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
 
+
+print("TensorFlow version:", tf.__version__)
